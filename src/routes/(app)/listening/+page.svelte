9d42850db42357791632d88524da7f7369116a92
Sam Randa <samrandaofficial@gmail.com>
2024-09-11 23:33:35 -0400

    Added register, login, and profile functionality

--- src/routes/(app)/listening/+page.svelte
+++ src/routes/(app)/listening/+page.svelte
@@ -1,1 +1,16 @@
+<script lang="ts">
+    import Song from './Song.svelte';
+    let songs: {
+        path: string
+        title: string
+        artist: string
+        album: string
+    }[] = []
+</script>
+
 <h1>Listening Data</h1>
+<div class="songs">
+    {#each songs as song}
+        <Song {song} />
+    {/each}
+</div>
---
+++ src/routes/(app)/listening/Song.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let song: {
+        path: string
+        title: string
+        artist: string
+        album: string
+    };
+</script>
+
+<div class="song">
+    <img src={song.path} alt={`The album art for ${song.album}.`}>
+    <p class="title">{song.title}</p>
+    <p class="artist">{song.artist}</p>
+    <p class="album">{song.album}</p>
+</div>

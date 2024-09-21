3468dbc5ca11606b2bd780bfa6d141cadbc202a3
Sam Randa <samrandaofficial@gmail.com>
2024-09-21 19:36:46 -0400

    Made validation login external

--- src/routes/(account-creation)/login/+page.server.ts
+++ src/routes/(account-creation)/login/+page.server.ts
@@ -1,18 +1,22 @@
-import { redirect } from '@sveltejs/kit'
-import type { Actions } from './$types'
+import { redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import { fail } from '@sveltejs/kit';
+import { validateAccountForm } from '../validateAccountForm';
 
 export const actions: Actions = {
   login: async ({ request, locals: { supabase } }) => {
-    const formData = await request.formData()
-    const email = formData.get('email') as string
-    const password = formData.get('password') as string
+    const formData = await request.formData();
+    const form = validateAccountForm("login", formData)
+    if (!form.valid) {
+      return fail(422, form.failures);
+    }
 
-    const { error } = await supabase.auth.signInWithPassword({ email, password })
+    const { error } = await supabase.auth.signInWithPassword(form.data);
     if (error) {
-      console.error(error)
-      redirect(303, '/login')
+      console.error(error);
+      redirect(303, '/login');
     } else {
-      redirect(303, '/profile')
+      redirect(303, '/profile');
     }
   },
 }

31bf92b4665260dd73afe205af3ba6aa13d46c34
Sam Randa <samrandaofficial@gmail.com>
2024-09-21 19:35:50 -0400

    Enhanced form with basic validation features

--- src/routes/(account-creation)/AccountForm.svelte
+++ src/routes/(account-creation)/AccountForm.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
+    import { enhance } from '$app/forms';
+    import type { SubmitFunction } from '@sveltejs/kit';
     export let type: "login" | "register";
+
+    let status = {
+        submitting: false,
+        missingEmail: false,
+        missingPassword: false,
+        passwordMismatch: false
+    }
+    const submit: SubmitFunction = () => {
+        status.submitting = true;
+
+        return async ({ result, update }) => {
+            if (result.type === "failure") {
+                console.log(result)
+                Object.assign(status, result.data)
+            } else {
+            }
+            await update();
+            status.submitting = false;
+        }; 
+    }
 </script>
 
 <div>
     <h1>welcome!</h1>
-    <form method="POST" action={"?/" + type}>
+    <form method="POST" action={"?/" + type} use:enhance={submit}>
         <fieldset>
-            <input type="text" name="email" id="email" placeholder="email">
-            <input type="password" name="password" id="password" placeholder="password">
+            <input type="text" name="email" id="email" placeholder="email" disabled={status.submitting}>
+            <p>{status.missingEmail ? "please enter an email" : ""}</p>
+            <input type="password" name="password" id="password" placeholder="password" disabled={status.submitting}>
+            <p>{status.missingPassword ? "please enter a password" : ""}</p>
             {#if type === "register"}
-                <input type="password" name="confirm_password" id="confirm_password" placeholder="confirm password">
+                <input type="password" name="confirm_password" id="confirm_password" placeholder="confirm password" disabled={status.submitting}>
+                <p>{status.passwordMismatch ? "passwords must match" : ""}</p>
             {/if}
         </fieldset>
         <fieldset>
@@ -37,5 +62,11 @@
 
     fieldset {
         gap: 10px;
+    }
+
+    p {
+        height: 0.85em;
+        font-size: 0.85em;
+        margin-bottom: 10px;
     }
 </style>
